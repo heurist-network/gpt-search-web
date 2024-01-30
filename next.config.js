453c7d01c4bc0baddf0f95b2bfb388c8063f9b91
flashcode <laughingliangcl@gmail.com>
2024-01-30 16:17:24 +0800

    feat: config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+// const { i18n } = require('./next-i18next.config')
+const nextConfig = {
+  // i18n,
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
